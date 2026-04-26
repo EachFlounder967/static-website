7c6c3ecdadcd246181fd632c6851a612d774f7cf
EachFlounder967 <jarrad.m.billiot@gmail.com>
2026-04-26 07:10:27 -0500

    Added ability to access repo as source of website from local

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,16 +1,30 @@
 import os
 from markdown_blocks import markdown_to_html_node
+from pathlib import Path
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+
+        if os.path.isfile(from_path):
+            if not filename.endswith(".md"):
+                continue
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+
+
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
-    from_file = open(from_path, "r")
-    markdown_content = from_file.read()
-    from_file.close()
 
-    template_file = open(template_path, "r")
-    template = template_file.read()
-    template_file.close()
+    with open(from_path, "r") as from_file:
+        markdown_content = from_file.read()
+
+    with open(template_path, "r") as template_file:
+        template = template_file.read()
 
     node = markdown_to_html_node(markdown_content)
     html = node.to_html()
@@ -19,29 +33,21 @@ def generate_page(from_path, template_path, dest_path):
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
 
+    # Apply basepath to absolute links
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
+
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
-    to_file = open(dest_path, "w")
-    to_file.write(template)
+
+    with open(dest_path, "w") as to_file:
+        to_file.write(template)
 
 
 def extract_title(md):
     lines = md.split("\n")
     for line in lines:
         if line.startswith("# "):
-            return line[2:]
+            return line[2:].strip()
     raise ValueError("no title found")
-
-
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    for entry in os.listdir(dir_path_content):
-        entry_path = os.path.join(dir_path_content, entry)
-        if os.path.isdir(entry_path):
-            generate_pages_recursive(
-                entry_path, template_path, os.path.join(dest_dir_path, entry)
-            )
-        elif entry.endswith(".md"):
-            dest_file_name = entry[:-3] + ".html"
-            dest_file_path = os.path.join(dest_dir_path, dest_file_name)
-            generate_page(entry_path, template_path, dest_file_path)
